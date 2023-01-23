4907ee504ed881c8965f8f8e9c4245a73a84edcd
bukeme <basseyukemme@gmail.com>
2023-01-23 21:51:05 +0100

    wrapped up carousel 1

--- js/falcon-9-script.js
+++ js/falcon-9-script.js
@@ -1,6 +1,11 @@
 const firstSection = document.querySelector('section');
 const stats = document.querySelector('.falcon-9-section-b__stats');
 const statsTitle = document.querySelectorAll('.falcon-9-section-b__stats--item__title');
+const overviewCarousel = document.querySelector('.falcon-9-section-b__overview--carousel-container__carousel');
+const overviewCarouselBtns = document.querySelectorAll('.falcon-9-section-b__overview .carousel-btn');
+const overviewCarouselLeftBtn = document.querySelector('.falcon-9-section-b__overview--carousel-container__icon-container--icon-left');
+const overviewCarouselRightBtn = document.querySelector('.falcon-9-section-b__overview--carousel-container__icon-container--icon-right')
+const overviewCarouselIndicators = document.querySelectorAll('.falcon-9-section-b__overview--carousel-container__indicator-container--indicator');
 const overviewCarouselList = document.querySelectorAll('.falcon-9-section-b__overview--carousel-container__carousel--item');
 const carousel_1_navLink_1 = document.querySelectorAll('.falcon-9-section-b__overview--carousel-container__carousel--item__nav-menu--nav-item__nav-link.nav-menu-1__nav-item--nav-link')
 const carousel_1_navLink_2 = document.querySelectorAll('.falcon-9-section-b__overview--carousel-container__carousel--item__nav-menu--nav-item__nav-link.nav-menu-2__nav-item--nav-link')
@@ -49,6 +54,42 @@ function alignCarouselItems(list) {
 
 alignCarouselItems(overviewCarouselList);
 
+let pos1 = 0;
+
+function moveCarouselItems(pos, carousel, btns) {
+	btns.forEach(btn => btn.classList.remove('disabled'));
+	const carouselItem = carousel.children[pos];
+	carousel.style.transform = `translateX(-${carouselItem.style.left})`;
+	overviewCarouselIndicators.forEach(function(indicator) {
+		indicator.classList.remove('active');
+	});
+	overviewCarouselIndicators[pos].classList.add('active');
+	if (pos === 0) {
+		btns[0].classList.add('disabled')
+	} else if (pos === carousel.children.length - 1) {
+		btns[1].classList.add('disabled')
+	}
+};
+
+overviewCarouselLeftBtn.addEventListener('click', function() {
+	if (pos1 === 0) return;
+	pos1--;
+	moveCarouselItems(pos1, overviewCarousel, overviewCarouselBtns);
+});
+
+overviewCarouselRightBtn.addEventListener('click', function() {
+	if (pos1 === overviewCarousel.children.length - 1) return;
+	pos1++;
+	moveCarouselItems(pos1, overviewCarousel, overviewCarouselBtns);
+});
+
+overviewCarouselIndicators.forEach(function(indicator) {
+	indicator.addEventListener('click', function() {
+		const pos = +this.dataset.pos;
+		pos1 = pos;
+		moveCarouselItems(pos, overviewCarousel, overviewCarouselBtns);
+	});
+});
 
 
 function displayNavContent(navLinks, contents) {
